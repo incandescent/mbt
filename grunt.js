9317346ba3c2a69752d9f75493e26e165f53afb0
Michal Kuklis <michal.kuklis@gmail.com>
2012-05-31 15:02:29 -0400

    adding gitignore

--- grunt.js
+++ grunt.js
@@ -1,35 +1,111 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
+
+  // load js paths
+  var jsFiles = require('./app/js/files.js').map(function (file) {
+    return "app/" + file;
+  });
 
   grunt.initConfig({
+
+    js: { files: jsFiles },
+
+    css: {
+      files: ["app/css/*.css"]
+    },
+
+    views: {
+      files: ["app/views/*.html"]
+    },
+
+    templates: {
+      files: ["app/templates/*.html.tmpl"]
+    },
+
     lint: {
-      files: ["grunt.js"]
+      files: ['app/js/*.js']
     },
-    watch: {
-      files: "<config:lint.files>",
-      tasks: "default"
-    },
+
     jshint: {
       options: {
+        asi: true,
         curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
+        eqeqeq: false,
+        expr: true,
+        forin: false,
         newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
+        laxcomma: true,
+        strict: true
+      }
+    },
+
+    htmlminifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: false,
+      removeRedundantAttributes: false,
+      removeEmptyAttributes: false,
+      removeOptionalTags: true
+    },
+
+    mocha: {
+      index: ['specs/index.html']
+    },
+
+    concat: {
+      'app/assets/app.js': '<config:js.files>',
+    },
+
+    jasmine: {
+      index: ['spec/javascripts/index.html']
+    },
+
+    lint: {
+      files: [
+        'app/js/config/*.js',
+        'app/js/helpers/*.js',
+        'app/js/models/*.js',
+        'app/js/views/*.js',
+        'app/js/modules/*.js']
+    },
+
+    // tasks configs
+    min: {
+      "app/assets/app.js": '<config:js.files>'
+    },
+
+    jst: {
+      "app/js/templates.js": '<config:templates.files>'
+    },
+
+    tmplmin: {
+      dist: {
+        src: '<config:views.files>',
+        dest: "app/index.html"
+      }
+    },
+
+    mincss: {
+      "app/assets/app.css": '<config:css.files>'
+    },
+
+    watch: {
+      templates: {
+        files: '<config:views.files>',
+        tasks: 'tmplmin'
       },
-      globals: {}
+      jst: {
+        files: '<config:templates.files>',
+        tasks: "jst"
+      },
+      mincss: {
+        files: '<config:css.files>',
+        tasks: "mincss"
+      }
     }
   });
 
-  // Load local tasks.
+  // Load local tasks
   grunt.loadTasks("tasks");
-
-  // Default task.
-  grunt.registerTask("default", "lint");
-};
+  grunt.loadNpmTasks('grunt-jasmine-task');
+  grunt.registerTask('default', 'watch');
+}
