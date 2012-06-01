d28bbc25c946f2795e49d820e447c34125a9a1ff
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-01 03:00:41 -0400

    fixing grunt

--- tasks/init/project/root/grunt.js
+++ tasks/init/project/root/grunt.js
@@ -1,87 +1,122 @@
-/*
- * grunt
- * https://github.com/cowboy/grunt
- *
- * Copyright (c) 2012 "Cowboy" Ben Alman
- * Licensed under the MIT license.
- * http://benalman.com/about/license/
- */
+module.exports = function (grunt) {
 
-module.exports = function(grunt) {
+  // load js paths
+  var jsFiles = require('./app/js/files.js').map(function (file) {
+    return "app/" + file;
+  });
 
-  // Project configuration.
+  var prodFiles = jsFiles.concat([
+      'app/js/config/envs/prod.js',
+      'phonegap/iphone/www/cordova-1.7.0.js']);
+
   grunt.initConfig({
-    test: {
-      all: ['test/**/*.js']
+    js: {
+      files: jsFiles,
+      prodFiles: prodFiles
     },
+
+    css: {
+      files: ["app/css/*.css"]
+    },
+
+    views: {
+      files: ["app/views/*.html"]
+    },
+
+    templates: {
+      files: ["app/templates/*.html.tmpl"]
+    },
+
     lint: {
-      all: ['grunt.js', 'lib/**/*.js', 'tasks/*.js', 'tasks/*/*.js', 'test/**/*.js']
+      files: ['app/js/*.js']
     },
-    docs: {
-      all: ['README.md', 'docs/*.md']
-    },
-    watch: {
-      scripts: {
-        files: '<config:lint.all>',
-        tasks: 'lint test'
-      },
-      docs: {
-        files: '<config:docs.all>',
-        tasks: 'docs'
-      }
-    },
+
     jshint: {
       options: {
+        asi: true,
         curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
+        eqeqeq: false,
+        expr: true,
+        forin: false,
         newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        es5: true
+        laxcomma: true,
+        strict: false,
+        funcscope: true
+      }
+    },
+
+    htmlminifier: {
+      removeComments: true,
+      collapseWhitespace: true,
+      collapseBooleanAttributes: false,
+      removeRedundantAttributes: false,
+      removeEmptyAttributes: false,
+      removeOptionalTags: true
+    },
+
+    concat: {
+      'app/assets/app.js': '<config:js.prodFiles>',
+    },
+
+    jasmine: {
+      index: ['spec/javascripts/index.html']
+    },
+
+    lint: {
+      files: [
+        'app/js/config/*.js',
+        'app/js/helpers/*.js',
+        'app/js/models/*.js',
+        'app/js/views/*.js',
+        'app/js/routes/*.js',
+        'app/js/modules/*.js',
+        'app/files.js',
+        'app/init.js',
+        'app/app.js']
+    },
+
+    // tasks configs
+    min: {
+      "app/assets/app.js": '<config:js.prodFiles>'
+    },
+
+    jst: {
+      "app/js/templates.js": '<config:templates.files>'
+    },
+
+    tmplmin: {
+      dist: {
+        src: '<config:views.files>',
+        dest: "app/index.html"
+      }
+    },
+
+    mincss: {
+      "app/assets/app.css": '<config:css.files>'
+    },
+
+    watch: {
+      templates: {
+        files: '<config:views.files>',
+        tasks: 'tmplmin'
       },
-      globals: {}
+      jst: {
+        files: '<config:templates.files>',
+        tasks: "jst"
+      },
+      mincss: {
+        files: '<config:css.files>',
+        tasks: "mincss"
+      }
+    },
+
+    release: {
+      "app/assets/app.js": '<config:js.files>'
     }
   });
 
-  // Default task.
-  grunt.registerTask('default', 'lint test docs');
-
-  // Process markdown documentation to make the world a better place.
-  grunt.registerMultiTask('docs', 'Tweak markdown documentation.', function() {
-    var files = grunt.file.expandFiles(this.file.src);
-    var processed = 0;
-    files.forEach(function(filepath) {
-      // Copying the file to itself allows it to be processed in-place.
-      grunt.file.copy(filepath, filepath, {process: function(src) {
-        // Add anchor links to all H2+ headers in .md document files.
-        var newSrc = src.replace(/(##+)\s+(.*?)\s*<a name=.*<\/a>/g, function(_, h, title) {
-          // Slugify the title text.
-          var slug = grunt.utils._.slugify(title.replace(/\./g, '-'));
-          // Put everything back together.
-          return h + ' ' + title + ' <a name="' + slug + '" href="#' + slug +
-            '" title="Link to this section">⚑</a>';
-        });
-        // Don't copy file if it didn't change.
-        if (newSrc === src) { return false; }
-        // Log and copy.
-        grunt.log.writeln('File "' + filepath + '" updated.');
-        processed++;
-        return newSrc;
-      }});
-    });
-
-    // Fail task if errors were logged.
-    if (this.errorCount) { return false; }
-
-    if (processed === 0) {
-      grunt.log.writeln('No documents updated.');
-    }
-  });
-
-};
+  // Load local tasks
+  grunt.loadTasks("tasks");
+  grunt.loadNpmTasks('grunt-jasmine-task');
+  grunt.registerTask('default', 'watch');
+}
