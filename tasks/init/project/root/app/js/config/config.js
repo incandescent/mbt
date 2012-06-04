610013f6e62799e98db80501fd5e4336cbcff390
Michal Kuklis <michal.kuklis@gmail.com>
2012-06-03 23:38:23 -0400

    fixing namespace

--- tasks/init/project/root/app/js/config/config.js
+++ tasks/init/project/root/app/js/config/config.js
@@ -1,16 +1,16 @@
 /*global module:false, exports:false */
 
-// test namespace
-if (typeof test === "undefined") {
-  var test = this.test = {};
+// {%= js_safe_name %} namespace
+if (typeof {%= js_safe_name %} === "undefined") {
+  var {%= js_safe_name %} = this.{%= js_safe_name %} = {};
 
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = test;
+    module.exports = {%= js_safe_name %};
   }
 }
 
 // config shared by all environments
-test.config = {
+{%= js_safe_name %}.config = {
   version: "0.0.1",
   env: "dev" // available envs: dev, test, prod
 };
